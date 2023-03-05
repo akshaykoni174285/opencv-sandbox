3d969e996acf9699bc8cce62bc22aa095a94d6f3
akshaykoni174285 <akshaykoni174285@gmail.com>
2023-03-05 18:01:12 +0530

    5 march

--- pothole.py
+++ pothole.py
@@ -1,57 +1,44 @@
-# import cv2 as cv 
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import sys
-# width = 500
-# height = 300
-# img = cv.imread('images/pothole_3.png')
-# img = cv.resize(img, (width, height))
-# img_color = cv.cvtColor(img,cv.COLOR_BGR2RGB)
-# cv.imshow("rgb",img_color)
-# # bluring the image
-# blur_img = cv.GaussianBlur(img,[3,3],cv.BORDER_DEFAULT)
-# cv.imshow("blur",blur_img)
-# cv.imshow("blur",blur_img)
-# applying the edge cascading
-# canny_img = cv.Canny(blur_img,100,200)
-# cv.imshow("blur",blur_img)
-# cv.imshow('pothole',img)
-# cv.imshow('pothole_outline',canny_img)
-# _,th1=cv.threshold(blur_img,100,255,1)
-
-# cv.imshow('thresholding',th1)
-
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib import colors
+# by akshay with love 
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 
 
 pothole = cv.imread('images/pothole_3.png')
-pothole = cv.cvtColor(pothole, cv.COLOR_BGR2RGB)
+# resizing the image 
 
-hsv_pothole = cv.cvtColor(pothole, cv.COLOR_RGB2HSV)
+pothole = cv.resize(pothole,(500,500),interpolation=cv.INTER_LINEAR)
+# converting the image in gray
+pothole_gray = cv.cvtColor(pothole, cv.COLOR_BGR2GRAY)
 
-# pothole_blur = cv.GaussianBlur(pothole,[3,3],cv.BORDER_DEFAULT)
+cv.imshow('gray',pothole_gray)
 
-grey_dark = (114,17,41)
-grey_light = (108,31,66)
+blur = cv.blur(pothole_gray,(3,3))
+g_blur = cv.GaussianBlur(pothole,(3,3),0)
+median = cv.medianBlur(pothole,5)
 
-mask = cv.inRange(hsv_pothole,grey_light,grey_dark)
-result = cv.bitwise_and(pothole,pothole,mask=mask)
+cv.imshow('blur',g_blur)
 
-# plt.subplot(1,2,1)
-# plt.imshow(mask,cmap="gray")
-# plt.subplot(1,2,2)
-# plt.imshow(result)
-# plt.show()
-cv.imshow('mask',mask)
+kernel = np.ones((3,3),np.uint8)
+
+erosion = cv.erode(median,kernel,iterations = 1)
+dilation = cv.dilate(erosion,kernel,iterations = 3)
+cv.imshow('dialation',dilation)
+closing = cv.morphologyEx(dilation, cv.MORPH_CLOSE, kernel)
+edges = cv.Canny(closing,127,360)
+cv.imshow('edges',edges)
+
+
+
+ret,threshold=cv.threshold(edges.copy(),0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+contours,_=cv.findContours(threshold,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
+
+cv.drawContours(pothole,contours,-1,(0,0,255),2)
+cv.imshow("Show",pothole)
 
 
 
 
-if cv.waitKey(0)==ord('q'):
+
+if cv.waitKey(0) == ord('q'):
     cv.destroyAllWindows()
-    
